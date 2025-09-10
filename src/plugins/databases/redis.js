a6a862fce7903204bc0860574de15b33ffd44465
Yaraslau Dzenisiuk <yaraslau.dzenisiuk@dataart.com>
2025-09-10 11:30:27 +0200

    [databases-for-node-js-developers] add redis connection

--- src/plugins/databases/redis.js
+++ src/plugins/databases/redis.js
@@ -1,15 +1,25 @@
 import fp from "fastify-plugin";
+import Redis from "ioredis";
 
 async function redisPlugin(fastify, config) {
-  let redisStatus = "disconnected";
+  let redisStatus = "no started";
 
-  // TODO: Connect to Redis and update the status
+  try {
+    const redis = new Redis(config.host, config.port);
+    redisStatus = "connected";
+    fastify.log.info("Connected to redis")
+    fastify.decorate("redis", redis);
+  } catch (err) {
+    fastify.log.error("Failed to connect to redis");
+    throw err;
+  }
+
   fastify.decorate("redisStatus", () => redisStatus);
 
   // Graceful shutdown
   fastify.addHook("onClose", async (fastifyInstance, done) => {
-    redisStatus = "disconnected";
-    // TODO: Close Redis connection
+    redisStatus = "closed";
+    await fastify.redis.quit();
     done();
   });
 }
